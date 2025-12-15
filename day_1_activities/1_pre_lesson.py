c2439de2e7403cf10d67caf57fec06caafa17271
chrisgb7 <gc6567824@gmail.com>
2025-12-15 20:07:50 +0000

    baka

--- day_1_activities/1_pre_lesson.py
+++ day_1_activities/1_pre_lesson.py
@@ -1,1 +1,33 @@
 # this is what we will use for the video intro to dictionaries
+#dictionary - a c ollection of {key:value} pairs orded and changeable no duplicates
+
+capitals = {"Usa":"Washington Dc",
+            "india": "new delhi", 
+            "china": "beijing,"
+            "russia": "Mascow"}
+
+# print(dir(capitals))
+# print(help(capitals))
+# print(capitals.get("japan"))
+
+if capitals.get("japan")
+    print("that capital exits")
+else:
+    print("that capital doestn exits")
+
+# capitals.update({"Germany": "Berlin"})
+# capitals.update({"USA": "Detroit"})
+# capitals.popitem()
+# capitals.clear()
+# print(capitals)
+
+keys= capitals.keys()
+for key in capitals.keys():
+    print(key)
+
+values =capital.values()
+for value in capitals.values():
+    print(value)
+
+for key,value in capitals.items():
+    print(f"{key}: {value}")
